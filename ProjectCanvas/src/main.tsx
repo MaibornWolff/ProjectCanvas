e519f0c302a2df7acc805da29c528937a7cbceba
IhsenBouallegue <bouallegueihsen@gmail.com>
2022-11-20 17:57:43 +0100

    feat: add mantine

--- ProjectCanvas/src/main.tsx
+++ ProjectCanvas/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./samples/node-api"
 import "styles/index.css"
+import { MantineProvider } from "@mantine/core"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
 
