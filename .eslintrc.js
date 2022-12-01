e9c90aeed4f8a69b74ca0b3f5709d30c4c284441
Ihsen <bouallegueihsen@gmail.com>
2022-12-01 16:58:04 +0100

    fix: remove redundant eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,17 +31,4 @@ module.exports = {
     },
   ],
   plugins: ["react"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "no-prototype-builtins": "off",
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    "react/destructuring-assignment": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
-    "no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true },
-    ],
-  },
 }
