dc96d245f11e06aa44406baa700bf6dce1333fcb
Ihsen <bouallegueihsen@gmail.com>
2022-12-12 20:15:13 +0100

    feat: add react router

--- project-canvas/src/App.tsx
+++ project-canvas/src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import { Layout } from "./components/layout"
 import { Login } from "./components/login"
+import { ProjectsView } from "./components/projects-view"
 
 export function App() {
-  return <Login />
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="projectsview" element={<ProjectsView />} />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
