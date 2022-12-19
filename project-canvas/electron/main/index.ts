485fecfe8c8cb144e06cdd54e9e5ace6aea12c51
Ihsen <bouallegueihsen@gmail.com>
2022-12-19 23:36:13 +0100

    feat: change window size

--- project-canvas/electron/main/index.ts
+++ project-canvas/electron/main/index.ts
@@ -47,7 +47,7 @@ async function createWindow() {
       contextIsolation: false,
     },
   })
-
+  win.setSize(1280, 720)
   if (process.env.VITE_DEV_SERVER_URL) {
     // electron-vite-vue#298
     win.loadURL(url)
