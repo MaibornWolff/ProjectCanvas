f57919caf2f506bac14f3c201311b701e77feaa9
Ihsen <bouallegueihsen@gmail.com>
2022-12-20 15:39:33 +0100

    feat: get project from api

--- project-canvas/src/components/projects-view/ProjectsView.tsx
+++ project-canvas/src/components/projects-view/ProjectsView.tsx
@@ -1,3 +1,24 @@
+import { Text } from "@mantine/core"
+import { useEffect, useState } from "react"
+
 export function ProjectsView() {
-  return <div>ProjectsView</div>
+  const [projects, setProjects] = useState<{ name: string; key: string }[]>()
+  const getProjects = async () => {
+    const data = await fetch(`${import.meta.env.VITE_EXTENDER}/projects`)
+    setProjects(await data.json())
+  }
+  useEffect(() => {
+    getProjects()
+  }, [])
+
+  return (
+    <div>
+      {projects?.map((project) => (
+        <Text key={project.key}>
+          name: {project.name} <br />
+          key: {project.key} <br />
+        </Text>
+      ))}
+    </div>
+  )
 }
