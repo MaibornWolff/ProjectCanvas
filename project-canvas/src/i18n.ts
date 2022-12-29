56e2d15d76ad7d8e856a6f4924b7b3e5a77a7ac1
Monotro <ch-huetter@gmx.de>
2022-12-29 13:23:28 +0100

    Add core functionality

---
+++ project-canvas/src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+
+import en from "./translations/en.json"
+import de from "./translations/de.json"
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      de: {
+        translation: de,
+      },
+    },
+    lng: "en", // set the default language
+    fallbackLng: "en", // set the fallback language
+  })
