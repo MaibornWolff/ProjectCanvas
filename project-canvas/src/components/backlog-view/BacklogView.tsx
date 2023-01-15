1c48fc51e7bcd4669154c0d8bdd8ceb62f3012d7
oussema18 <gib.masmoudi@gmail.com>
2023-01-15 21:14:29 +0100

    Fix: BacklogView

--- project-canvas/src/components/backlog-view/BacklogView.tsx
+++ project-canvas/src/components/backlog-view/BacklogView.tsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 import { DragDropContext } from "react-beautiful-dnd"
-import { Box, Button, Text, Container, Divider, Flex } from "@mantine/core"
-import { IconChevronLeft } from "@tabler/icons"
+import { Box, Divider } from "@mantine/core"
 import { Column } from "./Column"
 import { Pbi } from "./Item"
 import { resizeDivider } from "./resizeDivider"
 import { onDragEnd } from "./dndHelpers"
-import { useProjectStore } from "../projects-view/ProjectsTable"
 
 export function BacklogView() {
-  const zustandProject = useProjectStore((state) => state.selectedProject?.name)
-  const navigate = useNavigate()
+  const projectName = "Canvas"
   const BoardId = 1
   const [columns, setColumns] = useState(new Map())
   const [isLoading, setIsLoading] = useState(true)
@@ -47,7 +43,7 @@ export function BacklogView() {
       )
 
       await fetch(
-        `${import.meta.env.VITE_EXTENDER}/pbis?project=${zustandProject}`
+        `${import.meta.env.VITE_EXTENDER}/pbis?project=${projectName}`
       ).then(async (result) => {
         const pbis = await result.json()
         pbis.map() // DELETE
@@ -78,42 +74,30 @@ export function BacklogView() {
   return isLoading ? (
     <div>Loading...</div>
   ) : (
-    <Container>
-      <Flex align="center" gap="xl">
-        <Button
-          leftIcon={<IconChevronLeft />}
-          onClick={() => navigate("/projectsview")}
-          sx={{ flex: 1 }}
-        >
-          Back
-        </Button>
-        <Text sx={{ flex: 2 }}>project: {zustandProject}</Text>
-      </Flex>
-      <Box sx={{ height: "100%", width: "100%", display: "flex" }}>
-        <DragDropContext
-          onDragEnd={(dropResult) =>
-            onDragEnd({ ...dropResult, columns, updateColumn })
-          }
-        >
-          <Box className="left-panel" sx={{ padding: "5px", width: "50%" }}>
-            <Column col={columns.get("todo")!} />
-          </Box>
-          <Divider
-            className="resize-handle"
-            size="xl"
-            orientation="vertical"
-            sx={{
-              cursor: "col-resize",
-            }}
-          />
-          <Box className="right-panel" sx={{ padding: "5px", width: "50%" }}>
-            <Column col={columns.get("OUS Sprint 1")!} />
-            <Column col={columns.get("OUS Sprint 2")!} />
-            <Column col={columns.get("OUS Sprint 3")!} />
-            <Column col={columns.get("done")!} />
-          </Box>
-        </DragDropContext>
-      </Box>
-    </Container>
+    <Box sx={{ height: "100%", width: "100%", display: "flex" }}>
+      <DragDropContext
+        onDragEnd={(dropResult) =>
+          onDragEnd({ ...dropResult, columns, updateColumn })
+        }
+      >
+        <Box className="left-panel" sx={{ padding: "5px", width: "50%" }}>
+          <Column col={columns.get("todo")!} />
+        </Box>
+        <Divider
+          className="resize-handle"
+          size="xl"
+          orientation="vertical"
+          sx={{
+            cursor: "col-resize",
+          }}
+        />
+        <Box className="right-panel" sx={{ padding: "5px", width: "50%" }}>
+          <Column col={columns.get("OUS Sprint 1")!} />
+          <Column col={columns.get("OUS Sprint 2")!} />
+          <Column col={columns.get("OUS Sprint 3")!} />
+          <Column col={columns.get("done")!} />
+        </Box>
+      </DragDropContext>
+    </Box>
   )
 }
