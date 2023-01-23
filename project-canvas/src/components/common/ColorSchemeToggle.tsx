f18c10cc13f99a627cdb605accfc63dae7535da1
Ihsen <bouallegueihsen@gmail.com>
2023-01-23 22:10:12 +0100

    feat: update theme and add logo

---
+++ project-canvas/src/components/common/ColorSchemeToggle.tsx
@@ -0,0 +1,20 @@
+import { ActionIcon, useMantineColorScheme } from "@mantine/core"
+import { IconSun, IconMoonStars } from "@tabler/icons"
+
+export function ColorSchemeToggle({ ...props }) {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+  return (
+    <ActionIcon
+      variant="default"
+      onClick={() => toggleColorScheme()}
+      size={30}
+      {...props}
+    >
+      {colorScheme === "dark" ? (
+        <IconSun size={16} />
+      ) : (
+        <IconMoonStars size={16} />
+      )}
+    </ActionIcon>
+  )
+}
--- project-canvas/src/components/layout/LayoutHeader.tsx
+++ project-canvas/src/components/layout/LayoutHeader.tsx
@@ -1,80 +1,44 @@
-import {
-  ActionIcon,
-  Affix,
-  Box,
-  Button,
-  Group,
-  Header,
-  Menu,
-  useMantineColorScheme,
-} from "@mantine/core"
+import { Box, Button, Group, Header, Menu, Image } from "@mantine/core"
 import { showNotification } from "@mantine/notifications"
-import { IconSun, IconMoonStars, IconExternalLink } from "@tabler/icons"
 import { useNavigate } from "react-router-dom"
+import { ColorSchemeToggle } from "../common/ColorSchemeToggle"
 import { Logout } from "../login/jira-server/Logout"
 
 export function LayoutHeader() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
   const navigate = useNavigate()
   const LogoutSuccess = () => navigate("/")
   const LogoutFailed = () => {
     showNotification({
       title: "Failed to Logout",
-      message: "the caller is not authenticated.",
-      styles: (theme) => ({
-        root: {
-          backgroundColor: theme.colors.red[6],
-          borderColor: theme.colors.red[6],
-
-          "&::before": { backgroundColor: theme.white },
-        },
-
-        title: { color: theme.white },
-        description: { color: theme.white },
-        closeButton: {
-          color: theme.white,
-          "&:hover": { backgroundColor: theme.colors.blue[7] },
-        },
-      }),
+      message: "The use is not authenticated. Please restart the application.",
+      color: "red",
     })
   }
   return (
-    <Header height="60" p="sm">
+    <Header
+      height="60"
+      p="sm"
+      sx={(theme) => ({
+        borderBottom: `1px solid ${
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[4]
+            : theme.colors.gray[2]
+        }`,
+      })}
+    >
       <Box
         sx={(theme) => ({
           paddingLeft: theme.spacing.xs,
           paddingRight: theme.spacing.xs,
-          paddingBottom: theme.spacing.lg,
-          borderBottom: `1px solid ${
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[4]
-              : theme.colors.gray[2]
-          }`,
         })}
       >
-        <Group position="left" spacing="xl">
-          <ActionIcon
-            variant="default"
-            onClick={() => toggleColorScheme()}
-            size={30}
-          >
-            {colorScheme === "dark" ? (
-              <IconSun size={16} />
-            ) : (
-              <IconMoonStars size={16} />
-            )}
-          </ActionIcon>
-          <Affix position={{ top: 20, right: 20 }}>
-            <Button
-              color="dark"
-              onClick={() => {
-                Logout({ LogoutSuccess, LogoutFailed })
-              }}
-            >
-              Log out
-            </Button>
-          </Affix>
-
+        <Group spacing="xl">
+          <Image
+            src="/project_canvas_logo_sm.svg"
+            height={36}
+            width={36}
+            fit="contain"
+          />
           <Menu width={200} shadow="md">
             <Menu.Target>
               <Button>Projects</Button>
@@ -87,17 +51,20 @@ export function LayoutHeader() {
               >
                 Projects View
               </Menu.Item>
-
-              <Menu.Item
-                icon={<IconExternalLink size={14} />}
-                component="a"
-                href="https://mantine.dev"
-                target="_blank"
-              >
-                External link
-              </Menu.Item>
             </Menu.Dropdown>
           </Menu>
+
+          <ColorSchemeToggle size="34px" ml="auto" />
+
+          <Button
+            color="secondary"
+            variant="outline"
+            onClick={() => {
+              Logout({ LogoutSuccess, LogoutFailed })
+            }}
+          >
+            Log out
+          </Button>
         </Group>
       </Box>
     </Header>
