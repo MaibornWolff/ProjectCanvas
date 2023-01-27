ef79891ace9b005516ca1f82f305dbe36aeafc76
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-01-27 16:55:24 +0100

    feat: add react query

--- project-canvas/src/main.tsx
+++ project-canvas/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
 import { HashRouter } from "react-router-dom"
 import "styles/index.css"
 import { App } from "./App"
@@ -7,12 +8,16 @@ import { ThemeProvider } from "./ThemeProvider"
 import "./samples/node-api"
 import "./i18n/i18n"
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </HashRouter>
   </React.StrictMode>
 )
