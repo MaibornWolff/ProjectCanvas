08b72f10989b1472066d2bbf40bf216cbdd10aeb
oussema18 <gib.masmoudi@gmail.com>
2023-03-10 16:40:17 +0100

    eslint prettier test

--- project-canvas/src/i18n/i18next.d.ts
+++ project-canvas/src/i18n/i18next.d.ts
@@ -4,12 +4,12 @@ declare module "i18next" {
   interface CustomTypeOptions {
     defaultNS: typeof defaultNS
     resources: {
-      login: typeof resources[en]["login"]
-      login: typeof resources[de]["login"]
-      projectsView: typeof resources[en]["projectsView"]
-      projectsView: typeof resources[de]["projectsView"]
-      backlogView: typeof resources[en]["backlogView"]
-      backlogView: typeof resources[de]["backlogView"]
+      login: (typeof resources)[en]["login"]
+      login: (typeof resources)[de]["login"]
+      projectsView: (typeof resources)[en]["projectsView"]
+      projectsView: (typeof resources)[de]["projectsView"]
+      backlogView: (typeof resources)[en]["backlogView"]
+      backlogView: (typeof resources)[de]["backlogView"]
     }
   }
 }
