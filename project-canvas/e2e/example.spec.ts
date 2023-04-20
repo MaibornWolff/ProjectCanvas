2f544907103e9aa82aecf1a78ee956f0c700ee59
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-04-20 15:16:31 +0200

    fix: remove e2e env

--- project-canvas/e2e/example.spec.ts
+++ project-canvas/e2e/example.spec.ts
@@ -4,5 +4,5 @@ test("homepage has title and links to intro page", async () => {
   const app = await electron.launch({ args: [".", "--no-sandbox"] })
   const page = await app.firstWindow()
   expect(await page.title()).toBe("Project Canvas")
-  //   await page.screenshot({ path: "e2e/screenshots/example.png" })
+  await page.screenshot({ path: "e2e/screenshots/example.png" })
 })
