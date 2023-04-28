9bfe6899a3362cd2b2b135a5d3e17d8c971d6ef8
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-04-28 11:11:50 +0200

    refactor: use electron forge instead of electron builder

---
+++ project-canvas/electron/main.ts
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron")
+const path = require("path")
+
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string
+declare const MAIN_WINDOW_VITE_NAME: string
+
+// eslint-disable-next-line global-require
+if (require("electron-squirrel-startup")) {
+  app.quit()
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  })
+
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    )
+  }
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit()
+  }
+})
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
---
+++ project-canvas/forge.config.ts
@@ -0,0 +1,45 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-vite",
+      config: {
+        build: [
+          {
+            entry: "electron/main.ts",
+            config: "vite.main.config.ts",
+          },
+          {
+            entry: "electron/preload.ts",
+            config: "vite.preload.config.ts",
+          },
+        ],
+        renderer: [
+          {
+            name: "main_window",
+            config: "vite.renderer.config.ts",
+          },
+        ],
+      },
+    },
+  ],
+}
