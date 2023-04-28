75d066207c3c19322556052823af6b287b61ecbc
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-04-28 15:41:58 +0200

    fix: correctly load project extender

--- project-canvas/electron/main.ts
+++ project-canvas/electron/main.ts
@@ -1,11 +1,11 @@
-import { shell } from "electron"
+import { ipcMain, shell, app, BrowserWindow } from "electron"
 import path from "path"
-
-const { app, BrowserWindow } = require("electron")
-// const path = require("path")
+import { handleOAuth2 } from "./OAuthHelper"
 
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string
 declare const MAIN_WINDOW_VITE_NAME: string
+
+let mainWindow: BrowserWindow | null = null
 
 // eslint-disable-next-line global-require
 if (require("electron-squirrel-startup")) {
@@ -13,7 +13,8 @@ if (require("electron-squirrel-startup")) {
 }
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
+    icon: path.join(__dirname, "favicon.svg"),
     width: 1280,
     height: 720,
     webPreferences: {
@@ -25,7 +26,6 @@ const createWindow = () => {
 
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
-    mainWindow.webContents.openDevTools()
   } else {
     mainWindow.loadFile(
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
@@ -51,5 +51,23 @@ app.on("activate", () => {
     BrowserWindow.getAllWindows()[0].focus()
   } else {
     createWindow()
+  }
+})
+
+app.whenReady().then(() => {
+  ipcMain.on("start-oauth2", () =>
+    handleOAuth2(BrowserWindow.getAllWindows()[0]!)
+  )
+})
+
+app.whenReady().then(() => {
+  // Launch project extender fastify server
+  // eslint-disable-next-line global-require
+  require("project-extender")
+
+  // Launch dev tools in development tools
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL && mainWindow) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+    mainWindow.webContents.openDevTools({ mode: "undocked", activate: true })
   }
 })
