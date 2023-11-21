c635ea8e0d4a5c7bf70c8acc28915841acc4bc66
Maximilian Rüsch <78490564+maximilianruesch@users.noreply.github.com>
2023-11-21 23:02:09 +0100

    Migrate sprint functionality in server (#67)

--- electron/providers/jira-server-provider/JiraServerProvider.ts
+++ electron/providers/jira-server-provider/JiraServerProvider.ts
@@ -489,8 +489,46 @@ export class JiraServerProvider implements IProvider {
     })
   }
 
-  createSprint(sprint: SprintCreate): Promise<void> {
-    throw new Error("Method not implemented.")
+  createSprint({
+    name,
+    startDate,
+    endDate,
+    originBoardId,
+    goal,
+  }: SprintCreate): Promise<void> {
+    const offsetStartDate = this.offsetDate(startDate)
+    const offsetEndDate = this.offsetDate(endDate)
+
+    return new Promise((resolve, reject) => {
+      this.getAgileRestApiClient('1.0')
+        .post(
+          '/sprint',
+          {
+            name,
+            originBoardId,
+            ...(offsetStartDate && {
+              startDate: offsetStartDate,
+            }),
+            ...(offsetEndDate && {
+              endDate: offsetEndDate,
+            }),
+            ...(goal && { goal }),
+          }
+        )
+        .then(async () => { resolve() })
+        .catch((error) => {
+          let specificError = error
+          if (error.response) {
+            if (error.response.status === 403) {
+              specificError = new Error("The user does not have the necessary permissions")
+            } else if (error.response.status === 404) {
+              specificError = new Error("The Board does not exist or the user does not have the necessary permissions to view it")
+            }
+          }
+
+          reject(new Error(`Error creating sprint: ${specificError}`))
+        })
+    })
   }
 
   deleteIssue(issueIdOrKey: string): Promise<void> {
