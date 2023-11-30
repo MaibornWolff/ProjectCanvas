cd239e376304141daeb17f8a807458ab82214aa0
Benedict Teutsch <bene210@web.de>
2023-11-30 15:58:46 +0100

    Added Create Epic Button to Epic View

--- src/components/EpicView/EpicView.tsx
+++ src/components/EpicView/EpicView.tsx
@@ -1,14 +1,21 @@
-import {Group, Stack, Text, Title, Flex, ScrollArea, TextInput} from "@mantine/core";
+import {Group, Stack, Text, Title, Flex, ScrollArea, TextInput, Box, Button} from "@mantine/core";
 import {useNavigate} from "react-router-dom";
 import {useCanvasStore} from "../../lib/Store";
-import {IconSearch} from "@tabler/icons";
 import {useState} from "react";
+import {CreateIssueModal} from "../CreateIssue/CreateIssueModal";
+import {DraggableIssuesWrapper} from "../BacklogView/IssuesWrapper/DraggableIssuesWrapper";
+import {Issue, Sprint} from "../../../types";
 
 
 export function EpicView() : JSX.Element {
   const navigate = useNavigate()
   const projectName = useCanvasStore((state) => state.selectedProject?.name)
   const [search, setSearch] = useState("")
+  const [createIssueModalOpened, setCreateIssueModalOpened] = useState(false)
+  const [searchedissuesWrappers, setSearchedissuesWrappers] = useState(
+      new Map<string, { issues: Issue[]; sprint?: Sprint }>()
+  )
+
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const currentSearch = event.currentTarget.value
@@ -38,9 +45,7 @@ export function EpicView() : JSX.Element {
         </Group>
         <Title mb="sm">Epics</Title>
       </Stack>
-
-      <Flex sx={{ flexGrow: 1 }}>
-        <ScrollArea.Autosize
+      <ScrollArea.Autosize
           className="main-panel"
           maxHeight="calc(100vh - 230px)"
           w="100%"
@@ -48,10 +53,36 @@ export function EpicView() : JSX.Element {
           sx={{
             minWidth: "260px",
           }}
-        >
-          //TODO some component containing Epics
-        </ScrollArea.Autosize>
-      </Flex>
+      >
+        TODO wrapper containing Epic Cards (havent seen an Epic Card yet they might need work)
+        <Box mr="xs">
+          <Button
+              mt="sm"
+              mb="xl"
+              variant="subtle"
+              color="gray"
+              radius="sm"
+              display="flex"
+              fullWidth
+              onClick={() => setCreateIssueModalOpened(true)}
+              sx={(theme) => ({
+                justifyContent: "left",
+                ":hover": {
+                  background:
+                      theme.colorScheme === "dark"
+                          ? theme.colors.dark[4]
+                          : theme.colors.gray[4],
+                },
+              })}
+          >
+            + Create Epic
+          </Button>
+        </Box>
+        <CreateIssueModal
+            opened={createIssueModalOpened}
+            setOpened={setCreateIssueModalOpened}
+        />
+      </ScrollArea.Autosize>
     </Stack>
   )
 }
