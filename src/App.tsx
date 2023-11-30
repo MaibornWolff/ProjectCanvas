37ad38567cb744868d16281760eea4f4e7c71d78
Benedict Teutsch <bene210@web.de>
2023-11-30 15:37:39 +0100

    Added Button to open Epic View

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Layout } from "./components/layout"
 import { Login } from "./components/Login"
 import { ProjectsView } from "./components/ProjectsView"
 import { BacklogView } from "./components/BacklogView"
+import { EpicView } from "./components/EpicView"
 import { StoryMapView } from "./components/StoryMapView"
 import { StoryMapDashboard } from "./components/StoryMapView/StoryMapDashboard"
 
@@ -12,7 +13,8 @@ export function App() {
       <Route path="/" index element={<Login />} />
       <Route element={<Layout />}>
         <Route path="projectsview" element={<ProjectsView />} />
-        <Route path="backlogview" element={<BacklogView />} />
+          <Route path="backlogview" element={<BacklogView />} />
+          <Route path="epicview" element={<EpicView />} />
         <Route path="storymapview">
           <Route index element={<StoryMapDashboard />} />
           <Route path=":storyMapId" element={<StoryMapView />} />
