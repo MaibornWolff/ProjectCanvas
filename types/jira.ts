55173de065194a43203e19b06da9d48b8843060e
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2023-12-18 18:38:39 +0100

    Revert "commentSection implemented - edit/delete/add may take long(needs fixing)"
    This reverts commit fdd357544a3d40cdfd2441925cad17195c33003e.

--- types/jira.ts
+++ types/jira.ts
@@ -17,77 +17,7 @@ export interface JiraSprint {
   state: string
   name: string
 }
-// EpicIssue structure differs from normal Issue structure
-export interface JiraEpic {
-  key: string
-  fields: {
-    description: string
-    summary: string
-    creator: { name: string; displayName: string }
-    status: { name: string }
-    issuetype: { name: string }
-    customfield_10107: number
-    parent: { id: string; fields: { summary: string } }
-    epic: { name: string }
-    labels: string[]
-    assignee: {
-      displayName: string
-      avatarUrls: {
-        "16x16": string
-        "24x24": string
-        "36x36": string
-        "48x48": string
-      }
-    }
-    [rankCustomField: string]: string | unknown
-    subtasks: {
-      id: string
-      key: string
-      fields: {
-        summary: string
-      }
-    }[]
-    project: { id: string }
-    created: string
-    updated: string
-    comment: {
-      comments: [
-        {
-          id: string
-          author: {
-            accountId: string
-            avatarUrls: {
-              "48x48": string
-              "24x24": string
-              "16x16": string
-              "32x32": string
-            }
-            displayName: string
-          }
-          body: {
-            type: string
-            version: number
-            content: [
-              {
-                type: string
-                content: [
-                  {
-                    type: string
-                    text: string
-                  }
-                ]
-              }
-            ]
-          }
-          created: string
-          updated: string
-        }
-      ]
-    }
-    sprint?: JiraSprint
-    attachment?: Attachment[]
-  }
-}
+
 export interface JiraIssue {
   key: string
   fields: {
