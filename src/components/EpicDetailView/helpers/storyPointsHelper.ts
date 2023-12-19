45897bccf5cd4e91d2c06ac3c5fcc525c618156a
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2023-12-19 20:28:41 +0100

    Simplify story points helpers

--- src/components/EpicDetailView/helpers/storyPointsHelper.ts
+++ src/components/EpicDetailView/helpers/storyPointsHelper.ts
@@ -1,17 +1,13 @@
 import { Issue } from "../../../../types"
 
 export const storyPointsAccumulator = (issues: Issue[], status: string) =>
-  issues.reduce((accumulator, currentValue) => {
-    if (currentValue.storyPointsEstimate && currentValue.status === status) {
-      return accumulator + currentValue.storyPointsEstimate
-    }
-    return accumulator
-  }, 0)
+  issues.reduce(
+    (accumulator, currentValue) => accumulator + (currentValue.status === status ? currentValue.storyPointsEstimate : 0) ?? 0,
+    0,
+  )
 
 export const inProgressAccumulator = (issues: Issue[], status: string) =>
-  issues.reduce((accumulator, currentValue) => {
-    if (currentValue.status === status) {
-      return accumulator + 1
-    }
-    return accumulator
-  }, 0)
+  issues.reduce(
+    (accumulator, currentValue) => accumulator + (currentValue.status === status ? 1 : 0),
+    0,
+  )
