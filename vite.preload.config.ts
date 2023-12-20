606acb99291c58de336f6bf33789ea5c0e953bf4
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2023-12-20 22:50:53 +0100

    Remove electron-forge workarounds

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite"
-import { restart } from "./vite.main.config"
 
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({ plugins: [restart()] })
+export default defineConfig({})
