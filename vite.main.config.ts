833ff12efe39a1e0f3fe7346a897d7274a5851c6
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2023-12-20 23:01:26 +0100

    Revert "Remove electron-forge workarounds"
    This reverts commit 606acb99291c58de336f6bf33789ea5c0e953bf4.

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite"
 
+// https://github.com/electron/forge/issues/3208#issuecomment-1498476106
+export function restart() {
+  let config
+  return {
+    name: "electron-restart",
+    configResolved(_config) {
+      config = _config
+    },
+    closeBundle() {
+      if (config.mode === "production") {
+        return
+      }
+      process.stdin.emit("data", "rs")
+    },
+  }
+}
+
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  plugins: [restart()],
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
