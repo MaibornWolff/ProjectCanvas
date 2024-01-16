31ea76db586ced1b12bd5a9facc95befda77049f
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2024-01-16 20:57:38 +0100

    Refactor checkbox stacks

--- src/components/CreateExport/CreateExportModal.tsx
+++ src/components/CreateExport/CreateExportModal.tsx
@@ -1,22 +1,18 @@
-import { Dispatch, SetStateAction, useEffect,  useState } from "react"
-import {
-  Modal,
-  Stack,
-  Group,
-  Text, Button, Checkbox, Tooltip, Paper, MantineProvider, createTheme, ActionIcon,
-} from "@mantine/core"
-import {isEqual, uniqWith, sortBy} from "lodash";
-import {useQuery} from "@tanstack/react-query";
-import {IconInfoCircle} from "@tabler/icons-react";
-import {useCanvasStore} from "../../lib/Store";
-import {Issue} from "../../../types";
-import {exportIssues} from "./exportHelper";
-import {getIssuesByProject} from "../BacklogView/helpers/queryFetchers";
-import {StatusType} from "../../../types/status";
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { Modal, Stack, Group, Text, Button, Tooltip, Paper, ActionIcon } from "@mantine/core"
+import { uniqWith, sortBy } from "lodash";
+import { useQuery } from "@tanstack/react-query";
+import { IconInfoCircle } from "@tabler/icons-react";
+import { useCanvasStore } from "../../lib/Store";
+import { Issue } from "../../../types";
+import { exportIssues } from "./exportHelper";
+import { getIssuesByProject } from "../BacklogView/helpers/queryFetchers";
+import { StatusType } from "../../../types/status";
+import { CheckboxStack } from "./CheckboxStack";
 
 export function CreateExportModal({
-  opened,
-  setOpened,
+    opened,
+    setOpened,
 }: {
     opened: boolean
     setOpened: Dispatch<SetStateAction<boolean>>
@@ -48,7 +44,6 @@ export function CreateExportModal({
         'name',
       ],
     )
-    const allStatusNames = allStatus.map((status) => status.name);
 
     const allStatusNamesByCategory: { [key: string]: string[] } = {};
     allStatus.forEach((status) => {
@@ -56,22 +51,10 @@ export function CreateExportModal({
         allStatusNamesByCategory[status.statusCategory.name].push(status.name);
     });
 
-    const allIssueTypeNames = issueTypes ? issueTypes.map((issueType) => issueType.name!) : [];
-
-    const theme = createTheme({ cursorType: 'pointer' });
     const [includedIssueTypes, setIncludedIssueTypes] = useState<string[]>([]);
     const [includedIssueStatus, setIncludedIssueStatus] = useState<string[]>([]);
     const [issuesToExport, setIssuesToExport] = useState<Issue[]>([]);
 
-    const allTypesAndStatusSelected = isEqual(includedIssueTypes.sort(), allIssueTypeNames.sort())
-        && isEqual(includedIssueStatus.sort(), allStatusNames.sort());
-
-    function toggleInList(list: string[], value: string): string[] {
-        const index = list.indexOf(value);
-        return index < 0 ? [...list, value] : list.filter(containedValue => containedValue !== value);
-    }
-    const toggleIssueType = (type : string) => setIncludedIssueTypes(toggleInList(includedIssueTypes, type))
-    const toggleIssueStatus = (status : string) => setIncludedIssueStatus(toggleInList(includedIssueStatus, status))
 
     function calculateIssuesToExport() {
         setIssuesToExport(issues
@@ -94,6 +77,7 @@ export function CreateExportModal({
           }}
           centered
           withCloseButton={false}
+          size="40vw"
         >
           <Stack
             align="left"
@@ -115,7 +99,7 @@ export function CreateExportModal({
                   ta="center"
                   color="primaryBlue"
                   variant="filled"
-                  label="Only issues with corresponding types and status are exported"
+                  label="Only issues with corresponding types and a 'Done' status are exported"
                 >
                   <ActionIcon variant="subtle" ml="auto">
                     <IconInfoCircle />
@@ -123,73 +107,44 @@ export function CreateExportModal({
                 </Tooltip>
             </Group>
             <Paper shadow="md" radius="md" withBorder mb="5%">
-            <Group align ="top" justify="center" mb="5%">
-              <Stack align="center" mr="5%">
-                <Text size="md" fw={450} mt="7%" mb="10%" >Include Issue Types</Text>
-                  <Stack mt="-12%">
-                    <MantineProvider theme={theme}>
-                      {issueTypes && issueTypes.map((issueType) => (
-                        <Checkbox
-                          {...(!includedIssueTypes.includes(issueType.name!) && { c: "dimmed" })}
-                          key={issueType.id}
-                          label={issueType.name}
-                          checked={includedIssueTypes.includes(issueType.name!)}
-                          onChange={() => toggleIssueType(issueType.name!)}
-                        />
-                      ))}
-                    </MantineProvider>
-                  </Stack>
-              </Stack>
-              <Stack align="center">
-                <Text size="md" fw={450} mt="7%" mb="10%">Include Issue Status</Text>
-                  <Stack mt="-12%">
-                    <MantineProvider theme={theme}>
-                      {allStatus && allStatus.map((status) => (
-                        <Checkbox
-                          {...(!includedIssueStatus.includes(status.name) && { c: "dimmed" })}
-                          key={status.id}
-                          label={status.name}
-                          checked={includedIssueStatus.includes(status.name)}
-                          onChange={() => toggleIssueStatus(status.name)}
-                        />
-                      ))}
-                    </MantineProvider>
-                  </Stack>
-              </Stack>
-              <Stack align="center">
-                <Stack mt="-12%">
-                  <MantineProvider theme={theme}>
-                    <Checkbox
-                      c="dimmed"
-                      label="Select All"
-                      checked={allTypesAndStatusSelected}
-                      indeterminate={!allTypesAndStatusSelected && (includedIssueStatus.length > 0 || includedIssueTypes.length > 0)}
-                      onChange={() => {
-                        if (allTypesAndStatusSelected) {
-                          setIncludedIssueStatus([]);
-                          setIncludedIssueTypes([]);
-                        } else {
-                          setIncludedIssueTypes(allIssueTypeNames);
-                          setIncludedIssueStatus(allStatusNames);
-                        }
-                      }}
+              <Group align ="top" justify="center" mb="5%">
+                <Stack align="center" mr="5%">
+                  <Text size="md" fw={450} mt="7%" mb="10%" >Include Issue Types</Text>
+                  {issueTypes && (
+                    <CheckboxStack
+                      data={issueTypes.map((issueType) => ({
+                        value: issueType.name!,
+                        label: issueType.name!,
+                      }))}
+                      onChange={(includedTypes) => setIncludedIssueTypes(includedTypes)}
                     />
-                  </MantineProvider>
+                  )}
                 </Stack>
-              </Stack>
-            </Group>
+                <Stack align="center">
+                  <Text size="md" fw={450} mt="7%" mb="10%">Include Issue Status</Text>
+                  {allStatus && (
+                    <CheckboxStack
+                      data={allStatus.map((status) => ({
+                        value: status.name,
+                        label: status.name,
+                      }))}
+                      onChange={(includedStatus) => setIncludedIssueStatus(includedStatus)}
+                    />
+                  )}
+                </Stack>
+              </Group>
             </Paper>
             <Group>
               <Text size="90%" c="dimmed">
                 Issues to export: {issuesToExport.length}
               </Text>
-                <Button
-                  ml="auto"
-                  size="sm"
-                  onClick={() => { exportIssues(issuesToExport) }}
-                >
-                  Export CSV
-                </Button>
+              <Button
+                ml="auto"
+                size="sm"
+                onClick={() => { exportIssues(issuesToExport) }}
+              >
+                Export CSV
+              </Button>
             </Group>
           </Stack>
         </Modal>
