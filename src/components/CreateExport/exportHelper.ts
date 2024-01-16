2e13fe20ae5b2952eb10e5899acfc7b48107d09d
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2024-01-16 21:46:07 +0100

    Beautify export code and add new fields

--- src/components/CreateExport/exportHelper.ts
+++ src/components/CreateExport/exportHelper.ts
@@ -3,13 +3,16 @@ import {showNotification} from "@mantine/notifications";
 import {Issue} from "../../../types";
 import {ExportReply, ExportStatus} from "../../../electron/export-issues";
 
-export const exportIssues = (issues : Issue[]) => {
-  let data = "\"ID\",\"Name\",\"Start Date\"\n";
-  issues.forEach(
-    (issue) => {
-      data = data.concat(`"${issue.issueKey}","${issue.summary}","${issue.startDate}"\n`) // TODO more fields
-    });
-  ipcRenderer.send("exportIssues", data);
+export type ExportableIssue = Issue & { startDate: Date, endDate: Date, workingDays: number }
+
+export const exportIssues = (issues: ExportableIssue[]) => {
+  const data = ['"ID","Name","Start Date","End Date","Working days"']
+
+  issues.forEach((issue) => {
+    data.push(`"${issue.issueKey}","${issue.summary}","${issue.startDate?.toISOString()}","${issue.endDate?.toISOString()}",${issue.workingDays}`)
+  });
+
+  ipcRenderer.send("exportIssues", data.join("\n"));
   ipcRenderer.once(
     "exportIssuesReply",
     (_, reply: ExportReply) => {
