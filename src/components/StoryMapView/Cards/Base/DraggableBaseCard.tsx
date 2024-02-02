effac1fea76126fb5fb36f2e6c0c2d3e8c50f87c
Maximilian Rüsch <maximilian.ruesch@stud.tu-darmstadt.de>
2024-02-02 15:55:38 +0100

    Fix delete button display for story cards

--- src/components/StoryMapView/Cards/Base/DraggableBaseCard.tsx
+++ src/components/StoryMapView/Cards/Base/DraggableBaseCard.tsx
@@ -1,4 +1,4 @@
-import { PaperProps } from "@mantine/core";
+import { Box, PaperProps } from "@mantine/core";
 import { useMergedRef } from "@mantine/hooks";
 import { forwardRef, ReactNode } from "react";
 import { Draggable } from "@hello-pangea/dnd";
@@ -21,8 +21,21 @@ HTMLDivElement,
         {...provided.draggableProps}
         {...provided.dragHandleProps}
         {...props}
+        p="0"
       >
-        {children}
+        <Box
+          style={{
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          p="md"
+        >
+          {children}
+        </Box>
       </BaseCard>
     )}
   </Draggable>
