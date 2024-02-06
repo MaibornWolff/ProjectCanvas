bf92f11eae8f26883c26427f517a74772ff35a22
ayman <ayman.kallouz@gmail.com>
2024-02-06 22:05:00 +0100

    cleaned up the code

--- src/components/DetailView/Components/SplitIssue/SelectDropdownSearch.tsx
+++ src/components/DetailView/Components/SplitIssue/SelectDropdownSearch.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import { PillsInput, Pill, Combobox, CheckIcon, Group, useCombobox, ActionIcon } from "@mantine/core";
+import { ActionIcon, Combobox, InputBase, useCombobox } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 
 export function SelectDropdownSearch({
@@ -11,73 +11,59 @@ export function SelectDropdownSearch({
 }) {
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
-    onDropdownOpen: () => combobox.updateSelectedOptionIndex("active"),
   });
 
+  const [value, setValue] = useState<string | null>(null);
   const [search, setSearch] = useState("");
-  const [value, setValue] = useState<string[]>([]);
-  // const handleValueSelect = (val: string) => setValue((current) => (current.includes(val) ? current.filter((v) => v !== val) : [...current, val]));
 
-  const handleValueRemove = (val: string) => setValue((current) => current.filter((v) => v !== val));
+  const shouldFilterOptions = issueNames.every((item) => item !== search);
+  const filteredOptions = shouldFilterOptions
+    ? issueNames.filter((item) => item.toLowerCase().includes(search.toLowerCase().trim()))
+    : issueNames;
 
-  const values = value.map((item) => (
-    <Pill key={item} withRemoveButton onRemove={() => handleValueRemove(item)}>
+  const options = filteredOptions.map((item) => (
+    <Combobox.Option value={item} key={item}>
       {item}
-    </Pill>
+    </Combobox.Option>
   ));
-
-  const options = issueNames
-    .filter((item) => item.toLowerCase().includes(search.trim().toLowerCase()))
-    .map((item) => (
-      <Combobox.Option value={item} key={item} active={value.includes(item)}>
-        <Group gap="sm">
-          {value.includes(item) ? <CheckIcon size={12} /> : null}
-          <span>{item}</span>
-        </Group>
-      </Combobox.Option>
-    ));
 
   return (
     <Combobox
       store={combobox}
-      onOptionSubmit={() => {
+      onOptionSubmit={(val) => {
+        setValue(val);
+        setSearch(val);
         splitViewModalOpened(true);
       }}
     >
-      <Combobox.DropdownTarget>
-        <PillsInput onClick={() => combobox.openDropdown()}>
-          <Pill.Group>
-            {values}
+      <Combobox.Target>
+        <InputBase
+          rightSection={<Combobox.Chevron />}
+          leftSection={(
             <ActionIcon>
               <IconSearch size={14} />
             </ActionIcon>
-            <Combobox.EventsTarget>
-              <PillsInput.Field
-                onFocus={() => combobox.openDropdown()}
-                onBlur={() => combobox.closeDropdown()}
-                value={search}
-                variant="filled"
-                placeholder="Find issues"
-                onChange={(event) => {
-                  combobox.updateSelectedOptionIndex();
-                  setSearch(event.currentTarget.value);
-                }}
-                onKeyDown={(event) => {
-                  if (event.key === "Backspace" && search.length === 0) {
-                    event.preventDefault();
-                  }
-                }}
-              />
+          )}
+          value={search}
+          onChange={(event) => {
+            combobox.openDropdown();
+            combobox.updateSelectedOptionIndex();
+            setSearch(event.currentTarget.value);
+          }}
+          onClick={() => combobox.openDropdown()}
+          onFocus={() => combobox.openDropdown()}
+          onBlur={() => {
+            combobox.closeDropdown();
+            setSearch(value || "");
+          }}
+          placeholder="Search value"
 
-            </Combobox.EventsTarget>
-          </Pill.Group>
-
-        </PillsInput>
-      </Combobox.DropdownTarget>
+        />
+      </Combobox.Target>
 
       <Combobox.Dropdown>
         <Combobox.Options>
-          {options.length > 0 ? options : <Combobox.Empty>Nothing found...</Combobox.Empty>}
+          {options.length > 0 ? options : <Combobox.Empty>Nothing found</Combobox.Empty>}
         </Combobox.Options>
       </Combobox.Dropdown>
     </Combobox>
